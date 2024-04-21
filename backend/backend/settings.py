4ef3357d0c1410343480dcfa0e41d1ddb6d0e219
alaic19 <alaicapineda2@gmail.com>
2024-04-21 20:54:19 +0800

    6th

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-zg9gucjwm8e(1y^ybjji!ew1k0i$o3nftwz&=vpigef_+zk@@a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
